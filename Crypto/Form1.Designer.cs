97c725b347d2689f4c8521c73d58e3dd6ac70aec
SurOK <nikolay.gers@gmail.com>
2019-05-25 15:33:34 +0500

    Добавлен магма

--- Crypto/Form1.Designer.cs
+++ Crypto/Form1.Designer.cs
@@ -28,13 +28,117 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonCrypto = new System.Windows.Forms.Button();
+            this.textBoxIn = new System.Windows.Forms.TextBox();
+            this.textBoxOut = new System.Windows.Forms.TextBox();
+            this.buttonDecrypto = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonCrypto
+            // 
+            this.buttonCrypto.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonCrypto.Location = new System.Drawing.Point(3, 88);
+            this.buttonCrypto.Name = "buttonCrypto";
+            this.buttonCrypto.Size = new System.Drawing.Size(374, 31);
+            this.buttonCrypto.TabIndex = 0;
+            this.buttonCrypto.Text = "Кодировать";
+            this.buttonCrypto.UseVisualStyleBackColor = true;
+            this.buttonCrypto.Click += new System.EventHandler(this.ButtonCrypto_Click);
+            // 
+            // textBoxIn
+            // 
+            this.textBoxIn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxIn.Location = new System.Drawing.Point(383, 88);
+            this.textBoxIn.Name = "textBoxIn";
+            this.textBoxIn.Size = new System.Drawing.Size(375, 22);
+            this.textBoxIn.TabIndex = 1;
+            // 
+            // textBoxOut
+            // 
+            this.textBoxOut.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxOut.Location = new System.Drawing.Point(383, 274);
+            this.textBoxOut.Name = "textBoxOut";
+            this.textBoxOut.Size = new System.Drawing.Size(375, 22);
+            this.textBoxOut.TabIndex = 2;
+            // 
+            // buttonDecrypto
+            // 
+            this.buttonDecrypto.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonDecrypto.Location = new System.Drawing.Point(3, 274);
+            this.buttonDecrypto.Name = "buttonDecrypto";
+            this.buttonDecrypto.Size = new System.Drawing.Size(374, 36);
+            this.buttonDecrypto.TabIndex = 3;
+            this.buttonDecrypto.Text = "Раскодировать";
+            this.buttonDecrypto.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.buttonDecrypto, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxOut, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.buttonCrypto, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxIn, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.comboBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 38.98305F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 61.01695F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 53F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 177F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(761, 449);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(3, 3);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(374, 24);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label1.Location = new System.Drawing.Point(383, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(375, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Вписывать в 16 виде";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(761, 449);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonCrypto;
+        private System.Windows.Forms.TextBox textBoxIn;
+        private System.Windows.Forms.TextBox textBoxOut;
+        private System.Windows.Forms.Button buttonDecrypto;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
